9e285c217a90afbe7df77d1c97481e68a1fc1538
tenerifewonders <tenerifewonders@gmail.com>
2026-09-03 16:39:52 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "teide-fr-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./FR-TEIDE.html",
+  "./manifest.json",
+  "./teide.geojson",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
